677ee16e3665bfc565f24843d692fe0686d5489e
Patrycja <Patrycjaqtno05@gmail.com>
2021-01-07 19:02:34 +0100

    fist step complete'

--- main.js
+++ main.js
@@ -5,22 +5,41 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+let modal = document.getElementById("modal");
+let modal_button = document.getElementById("modal_show")
+let close_button = document.getElementById("close")
+
+modal_button.onclick = function() {
+  modal.style.display = "block"
+}
+
+close_button.onclick = function () {
+  modal.style.display = "none"
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
+  for (let i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "grid";  
   dots[slideIndex-1].className += " active"
 }
+
+
